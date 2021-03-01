7e7fb59d7c8bd69323a954067bbf4e15323b920a
alex <lewyk@hotmail.com>
2021-03-01 19:06:49 +0200

    fixed according to the task

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,64 @@
-import React from 'react';
-import FeedbackCounter from './components/FeedbackCounter';
+import React, { useState } from 'react';
+import FeedbackOptions from './components/FeedbackOptions';
+import Statistics from './components/Statistics';
+import Section from './components/Section';
+import Notification from './components/Notification';
 
-const App = () => (
-  <>
-    <FeedbackCounter />
-  </>
-);
+function App() {
+  const [state, setState] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const keysForBtns = Object.keys(state);
+  const { good, neutral, bad } = state;
+
+  const handleIncrement = ({ target: { name } }) => {
+    setState(prevState => ({
+      ...prevState,
+      [name]: prevState[name] + 1,
+    }));
+  };
+
+  const countTotalFeedback = () => {
+    let count = 0;
+    for (let value of Object.values(state)) {
+      count += value;
+    }
+    return count;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
+    return total ? Math.round((good / total) * 100) : 0;
+  };
+
+  return (
+    <div>
+      <Section title={'Please leave Feedback'}>
+        <FeedbackOptions
+          onLeaveFeedback={handleIncrement}
+          options={keysForBtns}
+        />
+      </Section>
+      {countTotalFeedback() ? (
+        <Section title={'Statistics'}>
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          />
+        </Section>
+      ) : (
+        <Section>
+          <Notification message="No feedback given"></Notification>
+        </Section>
+      )}
+    </div>
+  );
+}
 
 export default App;
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,25 @@
+import styles from './styles.module.css';
+
+const Values = ({ good, neutral, bad, total, positivePercentage }) => (
+  <div className={styles.statWrapper}>
+    <p className={styles.stat}>
+      Good: <span className={styles.statValue}>{good}</span>
+    </p>
+    <p className={styles.stat}>
+      Neutral: <span className={styles.statValue}>{neutral}</span>
+    </p>
+    <p className={styles.stat}>
+      Bad: <span className={styles.statValue}>{bad}</span>
+    </p>
+    <p className={styles.stat}>
+      Total:
+      <span className={styles.statValue}>{total}</span>
+    </p>
+    <p className={styles.stat}>
+      Positive Feedback:
+      <span className={styles.statValue}>{positivePercentage}</span>%
+    </p>
+  </div>
+);
+
+export default Values;
